83056a022e5f98fdd3931e052e69144b8ac96bf3
norang <norang1221@gmail.com>
2025-01-03 16:55:38 +0900

    Friend 구조체 생성

--- SecondWeek/UserTableViewController.swift
+++ SecondWeek/UserTableViewController.swift
@@ -7,13 +7,23 @@
 
 import UIKit
 
-class UserTableViewController: UITableViewController {
+struct Friend {
+    let name: String
+    let message: String
+    let profileImage: String
+}
 
-    var name = ["고래밥", "칙촉", "카스타드"] {
-        didSet {
-            tableView.reloadData()
-        }
-    }
+class UserTableViewController: UITableViewController {
+    
+    let friends: [Friend] = [
+        Friend(name: "민지", message: "고래밥 냠냠", profileImage: "star"),
+        Friend(name: "하니", message: "행복한 하루", profileImage: "pencil"),
+        Friend(name: "다니엘", message: "배고프당", profileImage: "star.fill")
+    ]
+
+//    var name = ["고래밥", "칙촉", "카스타드"]
+//    let message = ["고래밥 냠냠", "행복한 하루", "배고프당 카스타드 맛있겠다"]
+//    let profile = ["star", "pencil", "star.fill"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,12 +32,9 @@ class UserTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
-    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return friends.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -35,9 +42,9 @@ class UserTableViewController: UITableViewController {
         // as! 를 통해 별도 정의한 swift 파일을 매칭 해주어야 한다
         // 커스텀 셀의 경우에는 indexPath도 넣어주어야 함
      
-        cell.userImageView.backgroundColor = .yellow
-        cell.userNameLabel.text = name[indexPath.row]
-        cell.userMessageLabel.text = "상태 메세지"
+        cell.userImageView.image = UIImage(systemName: "\(friends[indexPath.row].profileImage)")
+        cell.userNameLabel.text = friends[indexPath.row].name
+        cell.userMessageLabel.text = "\(friends[indexPath.row].message)"
         
         cell.userNameLabel.font = .boldSystemFont(ofSize: 15)
         cell.userMessageLabel.font = .systemFont(ofSize: 15)
